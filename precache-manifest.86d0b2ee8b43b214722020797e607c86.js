f4bd660a3b59840ec661de24073500e96b95b2a9
alicifes <635023574@qq.com>
2022-03-28 09:03:18 +0800

    deploy

---
+++ precache-manifest.86d0b2ee8b43b214722020797e607c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4347969e0bee3f17e63",
+    "url": "/morney-website/css/app.54f8907d.css"
+  },
+  {
+    "revision": "a49961806a20cc90b8614d9a65550357",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "b4347969e0bee3f17e63",
+    "url": "/morney-website/js/app.1db333f0.js"
+  },
+  {
+    "revision": "9b6f767a9ac7b7f6f594",
+    "url": "/morney-website/js/chunk-vendors.2d1fbaf8.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "a2de33f4c26af7248b370bc1e0ae3399",
+    "url": "/morney-website/prcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
